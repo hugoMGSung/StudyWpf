f1a1fa8d21d5cd6a40ea6f61e3d1fd0fc7a0eea8
hugoMGSung <personar95@naver.com>
2020-07-27 13:37:48 +0900

    오전까지 작업 업로드

---
+++ MvvmApp/Helpers/Commons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MvvmApp.Helpers
+{
+    public class Commons
+    {
+        public static bool IsValidEmail(string email)
+        {
+            string[] parts = email.Split('@');
+            if (parts.Length != 2) return false;
+            return (parts[1].Split('.').Length >= 2);
+        }
+
+        public static int CalcAge(DateTime date)
+        {
+            int middle;
+            DateTime now = DateTime.Now;
+            if (now.Month <= date.Month && now.Day < date.Day)
+                middle = now.Year - date.Year - 1; // 생일이 안지났으면
+            else
+                middle = now.Year - date.Year; // 생일이 지났으면
+
+            return middle;
+        }
+    }
+}
---
+++ MvvmApp/Views/ShellView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MvvmApp.Views
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ShellView : MetroWindow
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
