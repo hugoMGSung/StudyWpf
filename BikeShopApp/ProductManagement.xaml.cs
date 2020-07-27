0cd9c6283470983b1c9d71b5e824492280597882
hugoMGSung <personar95@naver.com>
2020-07-27 13:38:45 +0900

    다시 업로드

---
+++ BikeShopApp/ProductManagement.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace BikeShopApp
+{
+    /// <summary>
+    /// ProductManagement.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ProductManagement : Page
+    {
+        ProductsFactory factory;
+
+        public ProductManagement()
+        {
+            InitializeComponent();
+
+            factory = new ProductsFactory();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            GrdProducts.ItemsSource = factory.FindProducts(TxtSearch.Text);
+        }
+
+        private void TxtSearch_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                GrdProducts.ItemsSource = factory.FindProducts(TxtSearch.Text);
+            }
+        }
+    }
+}
