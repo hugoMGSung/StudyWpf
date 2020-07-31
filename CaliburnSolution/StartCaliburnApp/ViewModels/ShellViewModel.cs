fad00f56b4b52aedaac3a551fc5d40df4fd8a1d4
hugoMGSung <personar95@naver.com>
2020-07-31 16:46:10 +0900

    Employees List Caliburn App

---
+++ CaliburnSolution/StartCaliburnApp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StartCaliburnApp.ViewModels
+{
+    public class ShellViewModel : PropertyChangedBase, IHaveDisplayName
+    {
+        string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get => !string.IsNullOrEmpty(Name);
+        }
+        public string DisplayName { get; set; }
+
+        public ShellViewModel()
+        {
+            DisplayName = "Basic Caliburn App";
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show($"Hello {Name}");
+        }
+    }
+}
